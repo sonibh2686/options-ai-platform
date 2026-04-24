0c7beae000e0219d0ceac397d4556f4a3b7c1afe
sonibh2686 <sonibh26@gmail.com>
2026-04-23 21:40:23 -0400

    fix react imports for vercel build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { analyzeTrade, fetchExpiries, fetchHistory, scanGlobal, scanToday } from "./api";
 import ScoreCard from "./components/ScoreCard";
 import SuggestionTable from "./components/SuggestionTable";
